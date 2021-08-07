534914a3239483f46c5141e7430e75503d852a37
DESKTOP-OKQQ9TH\Cong <dangcong1810@gmail.com>
2021-08-07 09:51:35 +0700

    first commit

---
+++ wp-content/plugins/post-carousel/admin/views/sp-framework/functions/helpers.php
@@ -0,0 +1,60 @@
+<?php if ( ! defined( 'ABSPATH' ) ) {
+	die; } // Cannot access directly.
+
+if ( ! function_exists( 'spf_array_search' ) ) {
+	/**
+	 *
+	 * Array search key & value
+	 *
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 */
+	function spf_array_search( $array, $key, $value ) {
+
+		$results = array();
+
+		if ( is_array( $array ) ) {
+			if ( isset( $array[ $key ] ) && $array[ $key ] == $value ) {
+				$results[] = $array;
+			}
+
+			foreach ( $array as $sub_array ) {
+				$results = array_merge( $results, spf_array_search( $sub_array, $key, $value ) );
+			}
+		}
+
+		return $results;
+
+	}
+}
+
+if ( ! function_exists( 'spf_pcp_microtime' ) ) {
+	/**
+	 * Between Microtime
+	 *
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 */
+	function sps_pcp_timeout( $timenow, $starttime, $timeout = 30 ) {
+
+		return ( ( $timenow - $starttime ) < $timeout ) ? true : false;
+
+	}
+}
+
+if ( ! function_exists( 'spf_wp_editor_api' ) ) {
+	/**
+	 *
+	 * Check for wp editor api
+	 *
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 */
+	function spf_wp_editor_api() {
+
+		global $wp_version;
+
+		return version_compare( $wp_version, '4.8', '>=' );
+
+	}
+}
